6e0a61b40dd1f593f5f5d581ca7dc85d438cbc93
mortyhawky <morty.hawky@outlook.com>
2026-07-24 11:41:46 +0200

    2026-07-24T11:39:06+02:00

--- c/codevault/threads/threads01.c
+++ c/codevault/threads/threads01.c
@@ -2,17 +2,61 @@
  * c /a/prog/c/codevault/threads
  * v threads01.c
  *
- * gcc -std=c23 -Wall -Wextra -Wpedantic \
-      -fsanitize=address,undefined      \
-      -g threads01.c -o threads01
- 
- * gdb ./threads01
- * b main   r   n
-*/---------------------------------------------------------------
+gcc -std=c23 \
+    -Wall -Wextra -Wpedantic \
+    -fsanitize=address,undefined \
+    -g threads01.c \
+    -pthread \
+    -o threads01
 
+ gdb ./threads01
+ b main   r   n
+ **---------------------------------------------------------------*/
+
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char** argv) {
+void *thrd_func(void *arg);
 
-    return 0;
+int main(void)
+{
+    // t1 and t2 are thread handles (thread identifiers).
+    // A handle is an opaque identifier used by the API to refer to a resource.
+    // In this case, it is a value stored in a pthread_t object.
+    // On this system pthread_t is typically:
+    //     typedef unsigned long pthread_t;
+    pthread_t t2, t2;
+
+    if (pthread_create(&t1, NULL, thrd_func, NULL) != 0) {
+        perror("pthread_create");
+        return EXIT_FAILURE;
+    }
+    if (pthread_create(&t2, NULL, thrd_func, NULL) != 0) {
+        perror("pthread_create");
+        return EXIT_FAILURE;
+    }
+
+    if (pthread_join(t1, NULL) != 0) {
+        perror("pthread_join");
+        return EXIT_FAILURE;
+    }
+
+    if (pthread_join(t2, NULL) != 0) {
+        perror("pthread_join");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+void *thrd_func(void *arg)
+{
+    (void)arg;  // suppress unused parameter warning
+    printf("Start of thread!\n");
+    sleep(2);
+    printf("End of thread!\n");
+
+    return NULL;
 }
