9782f5025d81996a4967b04e4b69e32d7c19fbf5
mortyhawky <morty.hawky@outlook.com>
2026-07-24 12:41:44 +0200

    2026-07-24T12:36:09+02:00

--- bash/ysap/e047-c11-s02/regex01.sh
+++ bash/ysap/e047-c11-s02/regex01.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ##################################################################
+# https://www.youtube.com/watch?v=kkzB3vat31U
 # pt47  Chapter 11  Section 02
 # Regular Expressions
 # Bash has it's own regex engine
@@ -23,10 +24,10 @@ else
     echo no match
 fi
 
+# 
 echo '==================='
-#regex='^.*\/(.*) - ([0-9]{4}-[0-9]{2}-[0-9]{2})\..*$'
 regex='^.*\/(.*) - ([0-9]{4}-[0-9]{2}-[0-9]{2})\..*$'
-for f in /a/prog/bash/ysap/e047-c11-s02/*; do
+for f in ./images/*; do
     if ! [[ $f =~ $regex ]]; then
         echo "$f didn't match pattern"
         continue
@@ -36,5 +37,3 @@ for f in /a/prog/bash/ysap/e047-c11-s02/*; do
     date=${BASH_REMATCH[2]}
     echo "$date: $name"
 done
-
-
