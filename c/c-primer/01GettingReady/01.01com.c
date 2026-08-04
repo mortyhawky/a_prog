26d7813619215239a9f93dcd148b3c74c04c9c86
mortyhawky <morty.hawky@outlook.com>
2026-08-04 16:02:16 +0200

    2026-08-02T06:57:49+02:00

--- c/c-primer/01GettingReady/01.01com.c
+++ c/c-primer/01GettingReady/01.01com.c
@@ -1,11 +1,11 @@
-/*  page 15     2026-07-07
+/*  page 15  2026-07-07  2026-08-04
  *  /a/prog/c/c-primer/01GettingReady/01.01example.c
  *  pm -S gcc gdb --needed
  *
  * Compile:
 gcc -std=c23 -Wall -Wextra -Wpedantic \
     -fsanitize=address,undefined \
-    -g 03-hello-c23.c
+    -g 01.01com.c
  *
  * Run:
 ./a.out
@@ -17,6 +17,7 @@ gdb ./a.out
 * n(ext)
 * print dogs
 * display dogs
+* display &dogs
 */
 
 #include <stdio.h>
@@ -24,15 +25,19 @@ gdb ./a.out
 int main(void)
 {
     // integer to store number of dogs (stack).
-    int dogs;
+    int dogs = 0;
+    /*
+     * dogs is an automatic local variable, 
+     * which resides on the call stack inside main()'s stack frame.
+     */
 
-    // Print Formated
-    printf("Ok! How many dogs do you have?\n");
-    // scan formated, read an int as decimal from stdin,
-    // into memory at the address of dogs.
-    // I.e. where the variable dogs live in memory (stack)
+    // Print Formated.
+    printf("How many dogs do you have? ");
+    // Scan Formated; read an int as decimal from stdin,
+    // into memory at the address of the variable dogs.
+    // I.e., where the variable dogs live in memory (stack)
     scanf("%d", &dogs);
-    printf("So you have %d dogs(s)!\n", dogs);
+    printf("So you have %d dog(s)!\n", dogs);
 
     // Why do we need &dogs in scanf and not in printf?
     // One could think that printf also would use the
@@ -41,16 +46,31 @@ int main(void)
     // For the function scanf to modify the dogs variable's value,
     // this has to be done through it's address (pointer)
     // &dogs IS a pointer to dogs. By evaluating the variable 
-    // dogs gives us its value e.g.,5).
+    // dogs gives us its value ,2).
     // Evaluating the expression &dogs gives its address
-    // e.g., 0x7ffd50, and that address value is a pointer.
+    // , 0x7b7ff7af0020, and that address-value is a pointer.
 
-    //Memory Address:   0x7ffd50
+    //Memory Address:   0x7b7ff7af0020  (stack)
     //Variable Name:    dogs
-    //Stored Value:     5
+    //Stored Value:     2
 
-    printf("%d", dogs);   //---> Sends 5
-    scanf("%d", &dogs);   //---> Sends 0x7ffd50 (so scanf knows WHERE to write)
+    //---> Send the value 2:
+    printf("printf %%d sends the value: %d\n", dogs);
+
+    printf("For scanf we provide a pointer to dogs; &dogs\n"
+           "&dogs is: %p\n", (void*)&dogs);
+
+    // Clearing the input buffer
+    //char c = '\0';
+    //while ( ( c = getchar() ) != '\n' && c != EOF ) {
+    //    // Do nothing - just consuming characters
+    //}
+    
+    printf("give me another number: ");   
+    scanf("%d", &dogs);   
+    // Sends , 0x7b7ff7af0020 (so scanf knows WHERE to write)
+
+    printf("Now &dogs at %p has the value %d\n", (void*)&dogs, dogs);
 
     return 0;
 }
