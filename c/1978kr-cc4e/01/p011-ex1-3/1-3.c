4126b8a26719c93899783e47a99495af84df7edc
mortyhawky <morty.hawky@outlook.com>
2026-08-10 07:06:32 +0200

    2026-08-10T03:40:27+02:00

--- c/1978kr-cc4e/01/p011-ex1-3/1-3.c
+++ c/1978kr-cc4e/01/p011-ex1-3/1-3.c
@@ -9,20 +9,30 @@ for f = 0, 20, ..., 300
 
 gcc -std=c23 -Wall -Wextra -Wpedantic \
     -fsanitize=address,undefined \
-        -g 03-hello-c23.c
+        -g 1-3.c
 */
+
+#define DEGREE_SIGN "\u00B0"
 
 int main(void) {
     int lower, upper, step;
     float fahr, celsius;
-    lower =   0;    /* lower limit of temperature table */
-    upper = 300;    /* upper limit */
-    step  =  20;    /* step size */
+    lower = -20;    /* lower limit of temperature table */
+    upper = 180;    /* upper limit */
+    step  =  25;    /* step size */
     fahr  = lower;
+
+    // %[flags][width][.precision]specifier
+    // % -      19     .1         lf
+    printf("12345678901234567890123456789012345678901234567890\n");
+    printf("         1         2         3         4         5\n");
+    printf("%s   %s\n",
+        "Fahrenheit (" DEGREE_SIGN "F)",
+        "Celsius (" DEGREE_SIGN "C)");
 
     while (fahr <= upper) {
         celsius = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%4.0f %6.1f\n", fahr, celsius);
+        printf("%10.1f%15.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
