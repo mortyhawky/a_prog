0fc0aeeb2a6302fb6512e534df2e994e771bf18f
mortyhawky <morty.hawky@outlook.com>
2026-08-17 05:13:42 +0200

    2026-08-17T05:13:39+02:00

--- c/1978kr-cc4e/01/p011/c_011_01.c
+++ c/1978kr-cc4e/01/p011/c_011_01.c
@@ -1,9 +1,39 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+/*
+ * KRC 1978 page 011
+ * Converted to c23
+ * Fahrenheit-Celsius table
+ * Using the for statment
 
-main() /* Fahrenheit-Celsius table */
-{
-    int fahr;
+ * The unindentet gcc command is intentional
+ * To use copy-mode (Alt+c/v,  y,  Alt+p) in tmux.
 
-    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
-        printf("%4d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+gcc -std=c23 -Werror -Wall -Wextra -Wpedantic \
+    -fsanitize=address,undefined \
+    -g c_011_01.c && ./a.out
+*/
+
+
+#define DEGREE_SIGN "\u00B0"
+
+int main(void) {
+    printf("sizeof(char)    = %zu byte(s)\n", sizeof(char)    );
+    printf("sizeof(int8_t)  = %zu byte(s)\n", sizeof(int8_t)  );
+    printf("sizeof(int16_t) = %zu byte(s)\n", sizeof(int16_t) );
+    printf("sizeof(int32_t) = %zu byte(s)\n", sizeof(int32_t) );
+    printf("sizeof(int64_t) = %zu byte(s)\n", sizeof(int64_t) );
+    printf("sizeof(float)   = %zu byte(s)\n", sizeof(float)   );
+    printf("sizeof(double)  = %zu byte(s)\n", sizeof(double)  );
+
+    for (int16_t fahr = 60; fahr <= 270; fahr += 25) {
+        printf(" %05.1f %sF    %05.1f %sC\n", 
+               (float)fahr,    // cast required: %f needs a
+                 DEGREE_SIGN,  // float/double, fahr is int16_t
+                   (5.0 / 9.0) * (fahr - 32), 
+                     DEGREE_SIGN );
+    }
+
+    return EXIT_SUCCESS;
 }
