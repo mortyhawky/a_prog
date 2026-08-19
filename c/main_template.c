0de0672329e237197711544ad45b2907a339755a
mortyhawky <morty.hawky@outlook.com>
2026-08-19 14:47:08 +0200

    2026-08-19T14:36:01+02:00

--- c/main_template.c
+++ c/main_template.c
@@ -6,12 +6,15 @@ gcc -std=c23 -Wall -Wextra -Werror -Wpedantic \
     main_template.c -o a.out && ./a.out && echo "echo \$? = "$?
 */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>  // for printf
+#include <stdlib.h> // for EXIT_SUCCESS
+#include <stdint.h> // for fixed width integers
 
 int main(void) {
     printf("main_template.c\n");
-    printf("Using C23\n");
+    char lang[] = "C";
+    uint8_t version = 23;
+    printf("Using %s%u\n", lang, version);
 
     return EXIT_SUCCESS;
 }
