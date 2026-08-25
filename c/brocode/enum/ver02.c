519ac2f35e5007212d0a1a16c548597b4068ec6d
mortyhawky <morty.hawky@outlook.com>
2026-08-25 17:46:06 +0200

    2026-08-25T17:45:36+02:00

--- c/brocode/enum/ver02.c
+++ c/brocode/enum/ver02.c
@@ -7,10 +7,41 @@ gcc -std=c23 -Werror -Wall -Wextra -Wpedantic \
 
 #include <stdio.h>
 #include <stdlib.h>
+ 
+enum Status{
+    SUCCESS,    // 0
+    FAILURE,    // 1
+    PENDING     // 2
+};
 
-enum Status
+void connectStatus(enum Status stat);
 
 int main(void) {
 
+    enum Status status = PENDING;
+
+    connectStatus(-1);
+    connectStatus(status++);
+    connectStatus(status);
+
     return EXIT_SUCCESS;
+}
+
+void connectStatus(enum Status s){
+
+    switch(s){
+        case SUCCESS:
+            printf("Connected!\n");
+            break;
+        case FAILURE:
+            printf("Couldn't connect\n");
+            break;
+        case PENDING:
+            printf("Waiting for connection...\n");
+            break;
+        default:
+            printf("Unknown error!  s = %i\n" , s );
+    }  
+
+    //return status;
 }
