0d24f05c70a5175b68a4c445e4e5fe13f634d51c
mortyhawky <morty.hawky@outlook.com>
2026-08-28 07:14:48 +0200

    2026-08-28T05:46:51+02:00

--- c/morty/reverse_string/main.c
+++ c/morty/reverse_string/main.c
@@ -3,23 +3,72 @@
 
 gcc -std=c23 -Wall -Wextra -Werror -Wpedantic \
     -fsanitize=address,undefined -g3 \
-    main.c -o a.out && ./a.out ; echo "Exit Code ="$?
+    main.c -o a.out && ./a.out ; echo "Exit Code = "$?
+
+clang -std=c23 -Wall -Wextra -Werror -Wpedantic \
+      -fsanitize=address,undefined -g3 \
+      main.c -o a.out && ./a.out ; echo "Exit Code = "$?
+
 */
 
-#include <stdio.h>  // for printf
-#include <stdlib.h> // for EXIT_SUCCESS
-#include <stdint.h> // for fixed width integers
-#include <inttypes.h> // for fixed width integers
+// len = 0:  i < 0 / 2 = 0
+//           no iterations
+//           Don't need any swaps to reverse 0 characters
+//
+// len = 1:  i < 1 / 2 = 0
+//           no iterations
+//           Don't need any swaps to reverse 1 character
+//
+// len = 2:  i < 2 / 2 = 1
+//           one iteration
+//           With 2 characters, we need only one swap.
+//
+// len = 3:  i < 3 / 2 = 1
+//           one iteration
+//           With 3 characters, we need only one swap.
+//           The middle character doesn't need to move.
+//
+// len = 4:  i < 4 / 2 = 2
+//           two iterations
+//           With 4 characters, we need two swaps.
+//
+// len = 5:  i < 5 / 2 = 2
+//           two iterations
+//           With 5 characters, we need two swaps.
+//           The middle character doesn't need to move.
+//
+//    length       swaps
+//      0           0
+//      1           0
+//      2           1
+//      3           1
+//      4           2
+//      5           2
+//      6           3
+//      7           3
+//      8           4
+
+#include <stdio.h>    // for printf
+#include <stdlib.h>   // for EXIT_SUCCESS
+#include <string.h>   // for strlen
 
 int main(void) {
-    printf("main_template.c\n");
-    char lang[] = "C";
-    uint8_t version = 23;
-    printf("Decimal     : %s%" PRIu8 "\n", lang, version);
-    printf("Hex         : %s0x%02X\n", lang, version);
+    printf("Reverse a string, in place:\n");
 
-    int8_t winter_temp = -11;
-    printf("Winter temp : %" PRId8 "\n", winter_temp);
+    char string[] = "1234567";
+    printf("String to reverse..: '%s'\n", string);
+    size_t len = strlen(string);
+    printf("Length of string...: %02zu\n", len);
+
+    size_t i = 0;
+    for(i = 0; i < len / 2; i++) {
+        char tmp = string[i];
+        string[i] = string[len - 1 - i];
+        string[len - 1 - i] = tmp;
+    }
+
+    printf("Reversed string....: '%s'\n", string);
+    printf("Iterations.........: %02zu \n", i);
 
     return EXIT_SUCCESS;
 }
