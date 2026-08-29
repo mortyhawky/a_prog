93a3347e35dcd4fadf797b6f459d5d7f5c27e4c2
mortyhawky <morty.hawky@outlook.com>
2026-08-29 08:24:53 +0200

    2026-08-29T07:16:29+02:00

--- c/main_template.c
+++ c/main_template.c
@@ -1,20 +1,23 @@
 /*
 c /a/prog/c/
 
+gcc     -std=c23 -Wall -Wextra -Werror -Wpedantic \
+        -fsanitize=address,undefined -g3 \
+        main_template.c -o a.out && ./a.out; echo "Exit code = $?"
 
-gcc -std=c23 -Wall -Wextra -Werror -Wpedantic \
-    -fsanitize=address,undefined -g3 \
-    main_template.c -o a.out && ./a.out; echo "Exit code = $?"
-
-clang -std=c23 -Wall -Wextra -Werror -Wpedantic \
-    -fsanitize=address,undefined -g3 \
-    main_template.c -o a.out && ./a.out; echo "Exit code = $?" 
+clang   -std=c23 -Wall -Wextra -Werror -Wpedantic \
+        -fsanitize=address,undefined -g3 \
+        main_template.c -o a.out && ./a.out; echo "Exit code = $?" 
 */
 
 #include <inttypes.h>   // for fixed width integers (uint8_t)
 #include <stdint.h>     // for fixed width integers (PRIu8)
 #include <stdio.h>      // for printf()
 #include <stdlib.h>     // for EXIT_SUCCESS/EXIT_FAILURE
+
+#define DEGREE_SIGN "\u00B0"
+#define MIN_TEMP    -11
+#define WINTER_TEMP  -9
 
 int main(void) {
     printf("main_template.c\n");
@@ -23,8 +26,12 @@ int main(void) {
     printf("Decimal     : %s%" PRIu8 "\n", lang, version);
     printf("Hex         : %s0x%02X\n", lang, version);
 
-    int8_t winter_temp = -11;
-    printf("Winter temp : %" PRId8 "\n", winter_temp);
+    int8_t winter_temp = WINTER_TEMP;
+    if ( winter_temp < MIN_TEMP ) {
+        // Exit code = 1 if winter_temp less than MIN_TEMP
+        return EXIT_FAILURE;
+    }
+    printf("Winter temp : %" PRId8 " %sC\n", winter_temp, DEGEEN_SIGN);
 
     return EXIT_SUCCESS;
 }
