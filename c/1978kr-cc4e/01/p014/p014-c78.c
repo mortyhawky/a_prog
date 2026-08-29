71a6bb3103790b1669167911483f0bd3d2d6a243
mortyhawky <morty.hawky@outlook.com>
2026-08-29 08:50:09 +0200

    2026-08-29T07:16:29+02:00

---
+++ c/1978kr-cc4e/01/p014/p014-c78.c
@@ -0,0 +1,19 @@
+#include <stdio.h>                                  /* c_014_01 */
+/*
+c /a/prog/c/1978kr-cc4e/01/p014/
+
+gcc p014-c78.c -o a.out -ansi && ./a.out; echo "$?"
+
+copy input to output; 1st version 
+*/
+
+main()
+{
+    int c;
+    c = getchar();
+
+    while (c != EOF) {
+        putchar(c);
+        c = getchar();
+    }
+}
