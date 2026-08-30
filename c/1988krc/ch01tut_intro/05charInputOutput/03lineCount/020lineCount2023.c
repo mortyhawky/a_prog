095c56ed77afbef536f813732a9c373ee07f01c5
mortyhawky <morty.hawky@outlook.com>
2026-08-30 16:21:32 +0200

    2026-08-30T11:24:43+02:00

--- c/1988krc/ch01tut_intro/05charInputOutput/03lineCount/020lineCount2023.c
+++ c/1988krc/ch01tut_intro/05charInputOutput/03lineCount/020lineCount2023.c
@@ -2,19 +2,19 @@
 
 c /a/prog/c/1988krc/ch01tut_intro/05charInputOutput/03lineCount
 
-gcc     019charCounting2023ver2.c -o a.out            \
+gcc     020LineCount2023.c -o a.out            \
         -std=c23                            \
         -Wall -Wextra -Werror -Wpedantic    \
         -fsanitize=address,undefined        \
         -g3 -O0                             \
-        && cat 019charCounting2023ver2.c | ./a.out; printf "\n*-> Exit code = $? <-*\n\n"
+        && cat 020LineCount2023.c | ./a.out; printf "\n*-> Exit code = $? <-*\n\n"
 
-clang   019charCounting2023ver2.c -o a.out            \
+clang   020LineCount2023.c -o a.out            \
         -std=c23                            \
         -Wall -Wextra -Werror -Wpedantic -Wno-parentheses   \
         -fsanitize=address,undefined        \
         -g3 -O0                             \
-        && cat 019charCounting2023ver2.c | ./a.out; printf "\n*-> Exit code = $? <-*\n\n"
+        && cat 020LineCount2023.c | ./a.out; printf "\n*-> Exit code = $? <-*\n\n"
 
  * 3 ways to test this program:
  *
@@ -22,10 +22,10 @@ clang   019charCounting2023ver2.c -o a.out            \
  *     Enter characters from stdin/keyboard; press Ctrl-D on an empty
  *     input line to signal EOF.
  *
- * cat 019charCounting2023.c | ./a.out
+ * cat 020LineCount2023.c | ./a.out
  *     cat writes the file contents to the program's stdin.
  *
- * ./a.out < 019charCounting2023ver2.c
+ * ./a.out < 020LineCount2023.c
  *     The shell redirects the file directly to the program's stdin.
 
  * ./a.out
@@ -45,11 +45,6 @@ clang   019charCounting2023ver2.c -o a.out            \
 
 int main(void) {
 
-    double nc = 0;
-    for ( nc = 0; getchar() != EOF; ++nc ) {
-        ;
-    }
-    printf("%.0f\n", nc);
 
     return EXIT_SUCCESS;
 }
