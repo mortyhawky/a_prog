2dc57b6066c63fea3556a21501f6306793dce685
mortyhawky <morty.hawky@outlook.com>
2026-09-15 19:23:46 +0200

    2026-09-15T19:21:47+02:00

--- c/2016giraffAcademy/14MadLibs/madlibs2.c
+++ c/2016giraffAcademy/14MadLibs/madlibs2.c
@@ -44,11 +44,12 @@ display/8db &color
 
 #define MAX_LEN 7
 // This give us effectivly MAX_LEN - 2 chars input
-// because \n gets converted to \0 and
+// because \n gets converted to \0 in trim_new_line
+//  and we always need a '\0' at the end.
 
 size_t trim_newline(char *str) {
-    uint8_t tell[]                = { 0, 1, 2, 3, 4 ,5 ,6, 7 };
-    printf("tell = %" PRIu8 "\n", tell[0] );
+    //uint8_t tell[]                = { 0, 1, 2, 3, 4 ,5 ,6, 7 };
+    //printf("tell = %" PRIu8 "\n", tell[0] );
     printf("trim_newline: remove \'\\n\' and \n");
     printf("make sure string got \'\\0\' at end. \n");
     printf("Input string (str) : %s \n", str);
@@ -62,25 +63,46 @@ size_t trim_newline(char *str) {
     return l;
 }
 
+void dump_mem(char *mem, size_t bytes) {
+    printf("bytes = %zu ", bytes);
+    printf("@ %p:", mem);
+    size_t i = 0;
+    for (i = 0; i < 8; i++) {
+        if ( mem[i] == 0 ) {
+            printf(" %02" PRIu8 "   ", 00 );
+            continue;
+        }
+        printf(" %02" PRIu8 "   ", mem[i] );
+    }
+    printf("\n\n");
+}
+
 int main()
 {
-    uint8_t teller[]              = { 0, 1, 2, 3, 4 ,5 ,6, 7 };
+    //uint8_t teller[]              = { 0, 1, 2, 3, 4 ,5 ,6, 7 };
     char    color       [MAX_LEN] = "";  // color
     //char   pluralNoun  [MAX_LEN] = "";
     //char   celebrity   [MAX_LEN] = "";
     size_t  len                   = 0;       // length after trim
     char    *s_fgets              = nullptr; // addr of str
 
-    printf("teller = %" PRIu8 "\n", teller[0] );
+    //dump_mem(teller, 8);
+
+    //printf("teller = %" PRIu8 "\n", teller[0] );
     printf("Enter a color.....: ");
     s_fgets = fgets(color, MAX_LEN, stdin);
     if ( !s_fgets ) {
         return EXIT_FAILURE;
     }
+    
+
     printf("s_fgets = %p\n", s_fgets);  // addr of string
     len = trim_newline(color);          // len of string
     printf("len after trim: %zu \n ", len );
     printf("color = %s \n ", color); 
+
+    
+    dump_mem( color , 8);
 
     return EXIT_SUCCESS;
 }
