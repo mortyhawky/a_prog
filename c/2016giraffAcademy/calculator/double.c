373bd997c46704096a711f8a1997401e7f685223
mortyhawky <morty.hawky@outlook.com>
2026-09-15 07:32:04 +0200

    2026-09-15T06:57:40+02:00

---
+++ c/2016giraffAcademy/calculator/double.c
@@ -0,0 +1,34 @@
+/*
+c /a/prog/c/2016giraffAcademy/calculator
+
+gcc       double.c -o a.out                         \
+          -g3 -O0 -std=c23                          \
+          -Wall -Wextra -Werror -Wpedantic          \
+          -fanalyzer -fsanitize=address,undefined   \
+          && ./a.out ; printf "Exit code = $?\n"
+
+clang     double.c -o a.out                         \
+          -g3 -O0 -std=c23                          \
+          -Wall -Wextra -Werror -Wpedantic          \
+          -fsanitize=address,undefined
+*/
+
+//#include <stdint.h>     // int16_t
+//#include <inttypes.h>   // PRId16
+#include <stdio.h>      // printf
+#include <stdlib.h>     // EXIT_SUCCESS
+
+int main()
+{
+    double num1 = 1;
+    double num2 = 2;
+    printf("Enter first number..: ");
+    scanf("%lf", &num1);                // double %lf, float %f
+    printf("Enter second number.: ");
+    scanf("%lf", &num2);
+
+    double sum = num1 + num2;
+    printf("Answer: %.2f\n", sum);      // double & float %f
+
+    return EXIT_SUCCESS;
+}
