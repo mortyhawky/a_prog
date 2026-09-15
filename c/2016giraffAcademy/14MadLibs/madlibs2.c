a89d2f5369057c87780a0017630a1ad5d3a3f4df
mortyhawky <morty.hawky@outlook.com>
2026-09-15 17:56:09 +0200

    2026-09-15T17:55:52+02:00

--- c/2016giraffAcademy/14MadLibs/madlibs2.c
+++ c/2016giraffAcademy/14MadLibs/madlibs2.c
@@ -1,28 +1,37 @@
 /*
 c /a/prog/c/2016giraffAcademy/14madLibs
 
-gcc       madlibs.c -o a.out             \
+gcc       madlibs2.c -o a.out             \
           -g3 -O0 -std=c23                          \
           -Wall -Wextra -Werror -Wpedantic          \
           -fanalyzer -fsanitize=address,undefined   \
           && ./a.out ; printf "Exit code = $?\n"
 
-clang     madlibs.c -o a.out             \
+clang     madlibs2.c -o a.out             \
           -g3 -O0 -std=c23                          \
           -Wall -Wextra -Werror -Wpedantic          \
           -fsanitize=address,undefined              \
           && ./a.out ; printf "Exit code = $?\n"
 
 
-gdb       a.out
+ ******  DEBUG  ******
+gcc       madlibs2.c -o a.out             \
+          -g3 -O0 -std=c23                          \
+          -Wall -Wextra -Werror -Wpedantic          \
+          -fanalyzer -fsanitize=address,undefined   \
+          && gdb --command gdb.file a.out
+
 b main
+b trim_newline
 r
+display MAX_LEN
+display s_trim
+display s_fgets
 display/8db &color
-display/8db &pluralNoun 
-display/8db &celebrity
-display len
-display status
-n
+
+#display len
+#display str
+#display/8xb str
 
 */
 
@@ -37,54 +46,68 @@ n
 // This give us effectivly MAX_LEN - 2 chars input
 // because \n gets converted to \0 and
 
-int trim_newline(char *str) {
-    printf("trim_newline: -> remove '\n' \n");
-    printf("and make sure string is '\0' terminated \n");
+size_t trim_newline(char *str) {
+    printf("trim_newline: remove \'\\n\' and \n");
+    printf("make sure string got \'\\0\' at end. \n");
+    printf("Input string (str) : %s \n", str);
+    size_t l = strlen(str);
+    printf("len = %zu \n", len);
+    str[l-1] = '\0';
 
-    return EXIT_SUCCESS;
+    l = strlen(str);
+    printf("l = %zu \n", l);
+
+    return l;
+}
 
 int main()
 {
-    char color       [MAX_LEN] = "";
-    char pluralNoun  [MAX_LEN] = "";
-    char celebrity   [MAX_LEN] = "";
-    size_t len                 = 0;
-    char* status               = nullptr;
-
+    char   color       [MAX_LEN] = "";  // color
+    //char   pluralNoun  [MAX_LEN] = "";
+    //char   celebrity   [MAX_LEN] = "";
+    size_t    len                = 0;       // length after trim
+    char   *s_fgets              = nullptr; // addr of str
 
     printf("Enter a color.....: ");
-    
-    status = fgets(color, MAX_LEN, stdin);
-    printf("status = %p\n", status);
-    int t = trim_newline(color);
-
-    return EXIT_FAILURE;
-
-    if (  !(fgets(color, MAX_LEN, stdin) )  ) {
+    s_fgets = fgets(color, MAX_LEN, stdin);
+    if ( !s_fgets ) {
         return EXIT_FAILURE;
     }
-    len = strlen(color);
-    len -= 1;
-    color [ len ] = '\0';         // replace the newline
-    //color [ MAX_LEN - 1 ] = '\0'; // always '\0' at MAX_LEN
+    printf("status = %p\n", s_fgets);   // addr of string
+    len = trim_newline(color);   // status trim_newline 0=OK
+    printf("trim_status = %d \n" , s_trim );
+
     
-//    printf("Enter plural noun.: ");
-//    if ( !fgets(pluralNoun, MAX_LEN, stdin) ) {
-//        return EXIT_FAILURE;
-//    }
-//    len = strlen(pluralNoun);
-//    len -= 1;
-//    pluralNoun[ len ] = '\0';
-//
-//    // celebrity
-//    printf("Enter celebrity...: ");
-//    fgets(celebrity , MAX_LEN, stdin);
-//    len = strlen(celebrity);
-//    celebrity [ len - 1 ] = '\0';
-
-    printf("Roses are %s \n", color      );
-    printf("%s are %s    \n", pluralNoun, color );
-    printf("I love %s    \n", celebrity  );
-
     return EXIT_SUCCESS;
 }
+
+
+
+////     if (  !(fgets(color, MAX_LEN, stdin) )  ) {
+////         return EXIT_FAILURE;
+////     }
+////     len = strlen(color);
+////     len -= 1;
+////     color [ len ] = '\0';         // replace the newline
+////     //color [ MAX_LEN - 1 ] = '\0'; // always '\0' at MAX_LEN
+////     
+//// //    printf("Enter plural noun.: ");
+//// //    if ( !fgets(pluralNoun, MAX_LEN, stdin) ) {
+//// //        return EXIT_FAILURE;
+//// //    }
+//// //    len = strlen(pluralNoun);
+//// //    len -= 1;
+//// //    pluralNoun[ len ] = '\0';
+//// //
+//// //    // celebrity
+//// //    printf("Enter celebrity...: ");
+//// //    fgets(celebrity , MAX_LEN, stdin);
+//// //    len = strlen(celebrity);
+//// //    celebrity [ len - 1 ] = '\0';
+//// 
+////     printf("Roses are %s \n", color      );
+////     printf("%s are %s    \n", pluralNoun, color );
+////     printf("I love %s    \n", celebrity  );
+//// 
+////     return EXIT_SUCCESS;
+//// }
