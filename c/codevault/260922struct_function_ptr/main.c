2b5e2805cbf0ab0daaf996c83baa97ee5afdecc3
mortyhawky <morty.hawky@outlook.com>
2026-09-22 10:39:49 +0200

    2026-09-22T10:39:07+02:00

--- c/codevault/260922struct_function_ptr/main.c
+++ c/codevault/260922struct_function_ptr/main.c
@@ -1,5 +1,5 @@
 /* cd into working directory
-c /a/prog/c/codevault/struct_function_ptr
+c /a/prog/c/codevault/260922struct_function_ptr/
 
  * Project; Compile & Run:
 clear && make && ./stru_func
@@ -23,27 +23,32 @@ struct Point {
 };
 typedef struct Point Point_t;
 
-Point_t get_middle_point(Point_t a, Point_t b);
+Point_t get_middle_point(const Point_t a, const Point_t b);
 
 int main(void) {
     printf("-> main:\n");
     puts("-");
 
-    Point_t p1 = {
+    const Point_t p1 = {
         .x = 1,  .y = 1, 
     };
 
-    Point_t p2 = {
+    const Point_t p2 = {
         .x = 3,  .y = 2, 
     };
 
     Point_t middle_point = get_middle_point(p1, p2);
     printf("middle_point = %0.3lf, %0.3lf \n", middle_point.x, middle_point.y );
 
+    const Point_t* ptr_p1 = &p1;
+    printf("ptr_p1 = %p \n", ptr_p1);
+    puts("-");
+
+
     return EXIT_SUCCESS;
 }
 
-Point_t get_middle_point(Point_t a, Point_t b) {
+Point_t get_middle_point(const Point_t a, const Point_t b) {
     // Calculate middle point of a.and b.
     // (a.x + b.x)/2.0
     // (a.y + b.y)/2.0
@@ -52,7 +57,7 @@ Point_t get_middle_point(Point_t a, Point_t b) {
     //    (a.y + b.y) / 2.0,
     //};
     
-    Point_t mp;
+    const Point_t mp;
     mp.x = (a.x + b.x) / 2.0;
     mp.y = (a.y + b.y) / 2.0;
 
