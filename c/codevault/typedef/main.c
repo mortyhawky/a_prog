81261022bda3353c3b3a0de347818d79982dbb8d
mortyhawky <morty.hawky@outlook.com>
2026-09-22 06:01:15 +0200

    2026-09-21T15:31:18+02:00

--- c/codevault/typedef/main.c
+++ c/codevault/typedef/main.c
@@ -8,7 +8,7 @@ clear && make && ./typedef
 clear && make clean && make && gdb --command gdb.file typedef
 
  * Project; Clean & Git
-make clean && gitall ; ll
+make clean && gitall || ll
 */
 
 #include <stdlib.h>
