37bc7e1e86235acb947de86015db7c928b873639
mortyhawky <morty.hawky@outlook.com>
2026-09-22 06:00:07 +0200

    2026-09-21T15:31:18+02:00

--- c/codevault/typedef/main.c
+++ c/codevault/typedef/main.c
@@ -7,24 +7,20 @@ clear && make && ./typedef
  * Project; Compile & Debug:
 clear && make clean && make && gdb --command gdb.file typedef
 
- * Project; Clean:
-make clean && ll
+ * Project; Clean & Git
+make clean && gitall
 */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include "typedef.h"
 
+typedef struct Point {
+    double x, y;
+} Point;
+
 int main(void) {
     printf("*->in main:\n");
-
-    //coordinate_t c = new_coord(1, 2, 3);
-    coordinate_t c = new_coord(1, 2, 3 );
-    show_coord(c);
-
-    coordinate_t scaled = scale_coordinate(c, 2);
-    show_coord(scaled);
-
 
     return EXIT_SUCCESS;
 }
--- c/codevault/typedef/typedef.c
+++ c/codevault/typedef/typedef.c
@@ -1,23 +1,3 @@
 #include <stdio.h>
 #include "typedef.h"
 
-coordinate_t new_coord(int x, int y, int z) {
-    coordinate_t coord = { .x = x, .y = y, .z = z };
-
-    return coord;
-}
-
-coordinate_t scale_coordinate(coordinate_t coord, int factor) {
-    coord.x *= factor;
-    coord.y *= factor;
-    coord.z *= factor;
-
-    return coord;
-}
-
-void show_coord(coordinate_t c) {
-    printf("%d \n", c.x);
-    printf("%d \n", c.y);
-    printf("%d \n", c.z);
-    puts("");
-}
