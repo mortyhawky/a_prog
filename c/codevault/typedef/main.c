c85713223f2acf83f1b2bdfe67d957b60adc8bd7
mortyhawky <morty.hawky@outlook.com>
2026-09-22 05:56:37 +0200

    2026-09-21T15:31:18+02:00

---
+++ c/codevault/typedef/main.c
@@ -0,0 +1,30 @@
+/* cd into working directory
+c /a/prog/c/codevault/typedef/
+
+ * Project; Compile & Run:
+clear && make && ./typedef
+
+ * Project; Compile & Debug:
+clear && make clean && make && gdb --command gdb.file typedef
+
+ * Project; Clean:
+make clean && ll
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "typedef.h"
+
+int main(void) {
+    printf("*->in main:\n");
+
+    //coordinate_t c = new_coord(1, 2, 3);
+    coordinate_t c = new_coord(1, 2, 3 );
+    show_coord(c);
+
+    coordinate_t scaled = scale_coordinate(c, 2);
+    show_coord(scaled);
+
+
+    return EXIT_SUCCESS;
+}
---
+++ c/codevault/typedef/typedef.h
@@ -0,0 +1,11 @@
+#pragma once
+
+typedef struct Coordinate {
+    int x;
+    int y;
+    int z;
+} coordinate_t;
+
+coordinate_t new_coord(int x, int y, int z);
+coordinate_t scale_coordinate(coordinate_t coord, int factor);
+void show_coord(coordinate_t c);
